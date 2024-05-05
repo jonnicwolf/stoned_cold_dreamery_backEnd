9190488519075c2f03e751a1bf88cb68ec2d6672
Fezz <jonnicwolf@gmail.com>
2024-05-05 10:46:16 -0400

    "Updated dependencies, controllers, and routes for app.js"
    Explanation:
    The code diff shows changes made to the app.js file, specifically in the dependencies, controllers, and routes. The `express()`

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
 // DEPENDENCIES
+const app = express();
 const cors = require("cors");
 const express = require("express");
-const productController = require("./controllers/productController");
-const usersController = require("./controllers/usersController");
-
-// CONFIGURATION
-const app = express();
+const products = require("./controllers/products");
 
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json()); // Parse incoming JSON
 
-app.use("/code", productController);
+app.use("/products", products);
+app.use("/users", users);
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();;
 });
 
-//ROUTES
+// ROUTES
 app.get("/", (req, res) => {
   res.send("Codesnip and Userinfo API");
 });
-
-app.use("/users", usersController);
 
 app.get("*", (req, res) => {
   res.status(404).send("404: Page not found");
 });
 
-// EXPORT
 module.exports = app;
